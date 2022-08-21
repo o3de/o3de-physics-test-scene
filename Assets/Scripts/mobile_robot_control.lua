a6ad35114e44901b80784ca83ab5040a92ea1e38
Paweł Budziszewski <pawel@arkanian.pl>
2022-08-21 18:33:32 +0200

    Cleaned the project and the lua script code

--- Assets/Scripts/mobile_robot_control.lua
+++ Assets/Scripts/mobile_robot_control.lua
@@ -1,33 +1,36 @@
 -- mobile_robot_control.lua 
 
 --[[
-Proof-of-concept mobile control physics
+    This is a proof-of-concept implementation of vehicle control physics. 
+    It allows controlling the speed and steering angle of the vehicle 
+    using the keyboard.
 --]]
-
-
 
 
 local mobile_robot_control = 
 {
     Properties =
     {
+        -- Entities that are needed by the control
         entities = 
         {
-        wheelLR = { default = EntityId() },
-        wheelRR = { default = EntityId() },
-        wheelLF = { default = EntityId() },
-        wheelRF = { default = EntityId() },
-        steeringL = { default = EntityId() },
-        steeringR = { default = EntityId() },
-        payload = { default = EntityId() },
+            wheelLR = { default = EntityId() },
+            wheelRR = { default = EntityId() },
+            wheelLF = { default = EntityId() },
+            wheelRF = { default = EntityId() },
+            steeringL = { default = EntityId() },
+            steeringR = { default = EntityId() },
+            payload = { default = EntityId() },
         },
         maxSteeringAngle = { 
             default = 0.5,
             suffix = "rad",
+            description = "The maximal angle of the steering"
         },
         maxSpeed = { 
             default = 2.0,
             suffix = "m/s",  
+            description = "The maximal speed"
         },
         frontWheelsDrive = { 
             default = false,
@@ -50,18 +53,19 @@ function mobile_robot_control:OnActivate()
      self.tickBusHandler:Connect()    
      
      self.InputNotificationBus = InputEventNotificationBus.Connect(self, InputEventNotificationId("mobile_robot_keys"))
-                    
-     self.tickTime = 0 
-     self.lastKey = 0
 
+     -- These vars indicates if arrow keys are pressed
      self.steeringWheelActive = false
      self.speedControlWheelActive = false
 
+     -- Values of speed and steering we want to maintain
      self.targetSteeringAngle = 0.0     
      self.targetSpeed = 0.0
 end
 
 function mobile_robot_control:_SetWheelSteeringAngle(entityid, targetAngle)
+    -- Applies torque to the steering knuckle trying to achieve targetAngle
+
     -- TODO make the condition below working
     --if not entityid==nil then
 
@@ -69,77 +73,77 @@ function mobile_robot_control:_SetWheelSteeringAngle(entityid, targetAngle)
         local knuckleRotation = TransformBus.Event.GetLocalRotation(entityid)['z']
         -- Calculate difference between target and current angles
         local dif = targetAngle - knuckleRotation
-        -- Calculate momentum to be applied
-        local momentum = dif*10.0
+        -- Calculate torque to be applied
+        local torque = dif*10.0
         -- We want to have more effect when dif is larger
-        momentum = momentum*momentum*momentum
+        torque = torque*torque*torque
 
         -- Obtain transformation of knuckle
         local tm = TransformBus.Event.GetWorldTM(entityid)
-        -- ...and use it to create momentum vector
-        local momentumVector = Transform.TransformVector(tm, Vector3(0, 0, momentum))
+        -- ...and use it to create torque vector
+        local torqueVector = Transform.TransformVector(tm, Vector3(0, 0, torque))
 
         -- Apply this vector to the knuckle
-        RigidBodyRequestBus.Event.ApplyAngularImpulse(entityid, momentumVector)
+        RigidBodyRequestBus.Event.ApplyAngularImpulse(entityid, torqueVector)
     --else
     --    Debug.Error("Steering objects not set")
     --end
-
 end
 
 
 function mobile_robot_control:SetSteering(targetAngle) -- rad
+    -- Rotates both steering knuckles to achieve targetAngle
     self:_SetWheelSteeringAngle(self.Properties.entities.steeringR, targetAngle)  
     self:_SetWheelSteeringAngle(self.Properties.entities.steeringL, targetAngle)  
 end
 
 
-function mobile_robot_control:_SetSpeedOneWheel(entityid, force)
+function mobile_robot_control:_SetSpeedOneWheel(entityid, torque)
+    -- Apply torque to one wheel
     local tm = TransformBus.Event.GetWorldTM(entityid)
-    local v = Transform.TransformVector(tm, Vector3(0,0,force))
-    RigidBodyRequestBus.Event.ApplyAngularImpulse(entityid, v)
+    local torqueVector = Transform.TransformVector(tm, Vector3(0,0,torque))
+    RigidBodyRequestBus.Event.ApplyAngularImpulse(entityid, torqueVector)
 end
 
 
 function mobile_robot_control:SetSpeed(targetSpeed, accelerationCoefficient)
+    -- Applies torque to driven wheels, trying to maintain targetSpeed
 
     local tm = TransformBus.Event.GetWorldTM(self.entityId)
     Transform.Invert(tm)
     local v = RigidBodyRequestBus.Event.GetLinearVelocity(self.entityId)
     v = Transform.TransformVector(tm, v)
     local currentSpeed = v["y"]
-    --Debug.Log(tostring(currentSpeed))
 
     dif = targetSpeed - currentSpeed
-    force = dif * accelerationCoefficient
-    if force>0 then 
-        force = force +0.1
-    elseif force<0 then
-        force = force -0.1
+    torque = dif * accelerationCoefficient
+    if torque>0 then 
+        torque = torque +0.1
+    elseif torque<0 then
+        torque = torque -0.1
     end
-    --Debug.Log("v="..tostring(currentSpeed).."  F="..tostring(force))
+
 
     if self.Properties.rearWheelsDrive then
-        self:_SetSpeedOneWheel(self.Properties.entities.wheelLR, force)
-        self:_SetSpeedOneWheel(self.Properties.entities.wheelRR, -force)
+        self:_SetSpeedOneWheel(self.Properties.entities.wheelLR, torque)
+        self:_SetSpeedOneWheel(self.Properties.entities.wheelRR, -torque)
     end
 
     if self.Properties.frontWheelsDrive then
-        self:_SetSpeedOneWheel(self.Properties.entities.wheelLF, force)
-        self:_SetSpeedOneWheel(self.Properties.entities.wheelRF, -force)
+        self:_SetSpeedOneWheel(self.Properties.entities.wheelLF, torque)
+        self:_SetSpeedOneWheel(self.Properties.entities.wheelRF, -torque)
     end
-
-
-
 end
 
 
 function mobile_robot_control:OnHeld  (value)
+    -- Keypress actions
 
     accCoeff = 0.05
     brakeCoeff = 0.5
 
     -- Process key press
+
     -- UP --
     if value == 1.0 then
         --Debug.Log("UP");
@@ -182,9 +186,6 @@ function mobile_robot_control:OnHeld  (value)
         v = Transform.TransformVector(tm, Vector3(0,0.5, 0))
         RigidBodyRequestBus.Event.ApplyAngularImpulse(self.Properties.entities.payload, v)
     end
-
-    self.lastKey = value
-
 end
 
 function mobile_robot_control:OnDeactivate()
@@ -193,20 +194,7 @@ end
 
 -- This callback is called every frame by the tick bus after this entity activates
 function mobile_robot_control:OnTick(deltaTime, timePoint)
-    self.tickTime = self.tickTime + deltaTime
-    -- Output some debug info (if needed), but not too often
-    if self.tickTime > 0.1 then
-       --local v = RigidBodyRequestBus.Event.GetAngularVelocity(self.Properties.wheel_L)
-       --local tm = TransformBus.Event.GetLocalTM(self.Properties.wheel_L)
-       --v = Transform.TransformVector(tm, v)
-       --Debug.Log(tostring(v))
 
-       -- local v = TransformBus.Event.GetLocalRotation(self.Properties.steering_L)
-       -- Debug.Log(tostring(v))
-
-
-       self.tickTime = 0
-    end    
 
    if self.steeringWheelActive then
         self.steeringWheelActive = false
@@ -221,21 +209,15 @@ function mobile_robot_control:OnTick(deltaTime, timePoint)
         self.targetSpeed = 0
    end 
 
-
    self:SetSteering(self.targetSteeringAngle)
    --self:SetSpeed(self.targetSpeed)
-
-
     local tm = TransformBus.Event.GetWorldTM(self.entityId)
     Transform.Invert(tm)
     local v = RigidBodyRequestBus.Event.GetLinearVelocity(self.entityId)
     v = Transform.TransformVector(tm, v)
     local currentSpeed = v["y"]
-    Debug.Log("v="..tostring(currentSpeed))
-
-
+    --Debug.Log("v="..tostring(currentSpeed))
 
 end
-
 
 return mobile_robot_control
